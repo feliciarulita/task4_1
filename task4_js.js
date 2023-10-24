4e051552ac84d26348d9981109a42ab4e7738d35
feliciarulita <147232185+feliciarulita@users.noreply.github.com>
2023-10-24 17:24:12 +0800

    Add files via upload

---
+++ task4_js.js
@@ -0,0 +1,68 @@
+
+    function myFunction() {
+    const myNodelist = document.querySelector("table");
+
+        myNodelist.style.width = "200px";
+    
+    }
+    function myFunction2() {
+    const myNodelist = document.querySelector("table");
+
+        myNodelist.style.width = "500px";
+    
+    }
+
+    function myFunction3() {
+    const myNodelist = document.querySelector("table");
+
+        myNodelist.style.border = "1px solid black ";
+        myNodelist.style.borderSpacing = "10px";
+    
+    }
+    function myFunction4() {
+    const myNodelist = document.querySelector("table");
+
+        myNodelist.style.border = "10px solid black";
+        myNodelist.style.borderSpacing = "10px";
+        
+    
+    }
+    function myFunction5() {
+        const myNodelist = document.querySelector("table");
+        myNodelist.style.border = "20px solid black";
+        myNodelist.style.borderSpacing = "20px";
+    }
+    function myFunction6() {
+        const myNodelist = document.querySelector("table");
+  
+            myNodelist.style.backgroundColor = "green";
+        
+    }
+    function myFunction7() {
+        const myNodelist = document.querySelector("table");
+
+            myNodelist.style.backgroundColor = "blue";
+        
+    }
+    function myFunction8() {
+        const myNodelist = document.querySelector("table");
+
+            myNodelist.style.backgroundColor = "purple";
+        
+    }
+    function myFunction9() {
+        const myNodelist = document.querySelector("table");
+
+            myNodelist.style.backgroundColor = "orange";
+        
+    }
+    function myFunction10() {
+        const myNodelist = document.querySelector("table");
+        myNodelist.style.width = "100%";
+        myNodelist.style.borderSpacing = "2px";
+        myNodelist.style.border = "1px solid black";
+        myNodelist.style.backgroundColor = "yellow";
+    
+    }
+        
+    
